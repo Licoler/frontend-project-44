efc73027c27a7066a0d975b0f3068a08f885232e
Альбек Халапов <albekhalapov@Albeks-MacBook-Pro.local>
2025-03-06 18:26:40 +0300

    twotask

--- src/src/cli.js
+++ src/src/cli.js
@@ -1,9 +1,4 @@
-import readlineSync from 'readline-sync';
 
-const greetUser = () => {
+export const greetUser = () => {
     console.log('Welcome to the Brain Games!');
-    const name = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + name + '!');
-};
-
-export default greetUser;
+  };
