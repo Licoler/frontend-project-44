70d6dfaf6effa178c5cc26d186ff4609e8fba73e
Neyilanau <halapovalbek@gmail.com>
2025-03-26 05:13:34 +0300

    theNinthTask

---
+++ bin/brain-prime.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+import { getRandomNumber } from '../src/utils.js'; 
+import startGame from '../src/index.js';
+
+const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (number) => {
+  if (number <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) return false;
+  }
+  return true;
+};
+
+const getQuestionAndAnswer = () => {
+  const number = getRandomNumber(1, 100);
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+  const question = number;
+  return [question, correctAnswer];
+};
+
+const checkAnswer = (userAnswer, correctAnswer) => {
+  if (userAnswer === correctAnswer) {
+    return "Correct!";
+  } else {
+    return `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'. Let's try again!`;
+  }
+};
+
+startGame(gameDescription, getQuestionAndAnswer, checkAnswer);
